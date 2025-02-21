6e437352754926c07260400e882b8f84bc1b6587
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-02-21 01:45:44 +0000

    Fix variable name

--- sync.py
+++ sync.py
@@ -4,9 +4,9 @@ import os
 url = "https://raw.githubusercontent.com/microsoft/vscode/main/extensions/json/syntaxes/JSONC.tmLanguage.json"
 response = requests.get(url)
 
-# Let's use the hash of the file to check if we need to update itq
-hash = response.headers['ETag']
-print(f"Hash: {hash}")
+# Let's use the hash of the file to check if we need to update it
+remote_hash = response.headers['ETag']
+print(f"Remote Hash: {remote_hash}")
 
 # Let's read the local file
 with open("JSONC.tmLanguage.json", "r") as file:
@@ -14,7 +14,7 @@ with open("JSONC.tmLanguage.json", "r") as file:
     local_hash = hash(local_data)
     print(f"Local Hash: {local_hash}")
 
-    if hash != local_hash:
+    if remote_hash != local_hash:
         print("Files are different. Updating local file")
         with open("JSONC.tmLanguage.json", "w") as file:
             file.write(response.text)
