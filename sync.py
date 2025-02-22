d773fd364c0e1517e003697bd45b5e47390e6a7c
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-02-22 06:13:40 +0000

    Move to syntaxes folder

--- sync.py
+++ sync.py
@@ -8,7 +8,7 @@ def run_update(commit_message):
     os.system("git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'")
     os.system("git config --global user.name 'github-actions[bot]'")
 
-    os.system("git add JSONC.tmLanguage.json")
+    os.system("git add syntaxes/JSONC.tmLanguage.json")
     os.system("git add retries.txt")
     os.system("git add disabled.txt")
     os.system(f"git commit -m '{commit_message}'")
@@ -98,14 +98,14 @@ remote_hash = hash(response.text)
 print(f"Remote Hash: {remote_hash}")
 
 # Let's read the local file
-with open("JSONC.tmLanguage.json", "r") as file:
+with open("syntaxes/JSONC.tmLanguage.json", "r") as file:
     local_data = file.read()
     local_hash = hash(local_data)
     print(f"Local Hash: {local_hash}")
 
     if remote_hash != local_hash:
         print("Files are different. Updating local file")
-        with open("JSONC.tmLanguage.json", "w") as file:
+        with open("syntaxes/JSONC.tmLanguage.json", "w") as file:
             file.write(response.text)
     else:
         print("Files are the same. No need to update")
