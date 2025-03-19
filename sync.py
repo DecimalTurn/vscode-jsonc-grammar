6613a785fb78e6817caf9bf9ff81370f1b118d58
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-03-19 19:49:01 -0400

    Update sync.py

--- sync.py
+++ sync.py
@@ -16,7 +16,7 @@ def run_update(commit_message):
 
 def format_grammar(data):
     # Turns out that the token used for the keys in the JSONC.tmLanguage.json file is "support.type" which 
-    # doesn't give a nice contrast with the string and other contant values.
+    # doesn't give a nice contrast with the string and other constant values.
     # Instead, we replace "support.type" with "entity.name.tag" to give a better contrast similar as what is used in the JSON grammar (https://github.com/Nixinova/NovaGrammars/blob/main/grammars/json.yaml-tmLanguage)
     data = data.replace('support.type', 'entity.name.tag')
     return data
